e172ef619a799d3a19c5718920b92f4e606db2a7
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2026-04-13 14:19:26 +0600

    feat: contains duplicate with O(n)

--- 01-arrays-and-hashing/contains-duplicate/solution.ts
+++ 01-arrays-and-hashing/contains-duplicate/solution.ts
@@ -1,13 +1,37 @@
 // O(n^2) time complexity
 // O(1) space complexity
 
+// function containsDuplicate(nums: number[]): boolean {
+//     for(let i = 0; i < nums.length; i++) {
+//         const currentNum = nums[i];
+//         for(let j = i + 1; j < nums.length; j++) {
+//             if(currentNum === nums[j]) {
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
+
+// Test cases
+// const test1 = [1, 2, 3, 1];
+// const test2 = [1, 2, 3, 4];
+// console.log(`Test 1 ([1,2,3,1]): ${containsDuplicate(test1)}`);
+// console.log(`Test 2 ([1,2,3,4]): ${containsDuplicate(test2)}`);
+
+
+
+// O(n) time complexity
+// O(n) space complexity
+
 function containsDuplicate(nums: number[]): boolean {
-    for(let i = 0; i < nums.length; i++) {
-        const currentNum = nums[i];
-        for(let j = i + 1; j < nums.length; j++) {
-            if(currentNum === nums[j]) {
-                return true;
-            }
+    const seen = new Set<number>()
+
+    for (let num of nums) {
+        if(seen.has(num)) {
+            return true
+        } else {
+            seen.add(num)
         }
     }
     return false;
