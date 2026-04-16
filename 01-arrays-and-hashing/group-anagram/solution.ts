58bdb7b298788bffaf99a272613ca65723a41bcd
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2026-04-16 12:42:00 +0600

    feat: group anagram

---
+++ 01-arrays-and-hashing/group-anagram/solution.ts
@@ -0,0 +1,13 @@
+function groupAnagrams(strs: string[]): string[][] {
+    const map = new Map<string, string[]>();
+    for(let i = 0; i < strs.length; i++) {
+        const sortedStr = strs[i].split("").sort().join("");
+        if(map.has(sortedStr)) {
+            map.get(sortedStr)!.push(strs[i])
+        } else {
+            map.set(sortedStr, [strs[i]])
+        }
+    }
+    return Array.from(map.values())
+};
+console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
