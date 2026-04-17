b5d2940ed793f5f43c51898084bbaf23d21dd3e6
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2026-04-17 12:57:37 +0600

    feat: group anagram with O(k * n)

--- 01-arrays-and-hashing/group-anagram/solution.ts
+++ 01-arrays-and-hashing/group-anagram/solution.ts
@@ -13,4 +13,29 @@ function groupAnagrams(strs: string[]): string[][] {
     }
     return Array.from(map.values())
 };
+
+// O(n * k) time complexity
+// O(n * k) space complexity
+function groupAnagramsOptimized(strs: string[]): string[][] {
+    const map = new Map<string, string[]>();
+    
+    for (const str of strs) {
+        const count = new Array(26).fill(0); // [0, 0, 0, ...]
+        
+        for (const char of str) {
+            count[char.charCodeAt(0) - 97]++; // [1, 0, 0, ...]
+        }
+        
+        const key = count.join("#"); // unique signature "1#0#0#..."
+        
+        if (!map.has(key)) { 
+            map.set(key, []); // create new array if not exists 
+        }
+        
+        map.get(key)!.push(str); // push the string to the array
+    }
+    
+    return Array.from(map.values());
+}
+
 console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
---
+++ 01-arrays-and-hashing/group-anagram/takeaway.md
@@ -0,0 +1,5 @@
+
+## Key Idea
+👉 Instead of sorting string
+
+“Count how many times each letter appears and use that as the key”
