71b5a191ab65d2144df49fd4c33d595425ede878
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2026-04-21 17:52:07 +0600

    feat: top k frequent elements with O(n)

--- 01-arrays-and-hashing/top-k-frequent/solution.ts
+++ 01-arrays-and-hashing/top-k-frequent/solution.ts
@@ -1,3 +1,6 @@
+// O(n log n) time complexity
+// O(n) space complexity
+
 function topKFrequent(nums: number[], k: number): number[] {
     const map = new Map<number, number>();
 
@@ -16,4 +19,39 @@ function topKFrequent(nums: number[], k: number): number[] {
  
 };
 
-console.log(topKFrequent([1, 2, 3, 3, 3, 3, 2, 2], 2))
+
+function topKFrequentBucketSort(nums: number[], k: number): number[] {
+    const freqMap = new Map<number, number>();
+
+    // Step 1: count frequency O(n)
+    for (const num of nums) {
+        freqMap.set(num, (freqMap.get(num) || 0) + 1); // Map(3) { 1 => 1, 2 => 3, 3 => 4 }
+    }
+
+    // Step 2: buckets O(n)
+    const buckets: number[][] = Array(nums.length + 1)
+        .fill(null)
+        .map(() => []); // [ [], [], [], [], [], [], [], [], [] ]
+        
+    // Step 3: fill buckets
+    for (const [num, freq] of freqMap) {
+        buckets[freq].push(num); //buckets [ [], [ 1 ], [], [ 2 ], [ 3 ], [], [], [], [] ]
+    }
+
+    // Step 4: collect top k O(n)
+    const result: number[] = [];
+
+    for (let i = buckets.length - 1; i >= 0; i--) {
+        for (const num of buckets[i]) {
+            result.push(num);
+            if (result.length === k) {
+                return result;
+            }
+        }
+    }
+
+    return result;
+}
+
+// console.log(topKFrequent([1, 2, 3, 3, 3, 3, 2, 2], 2))
+console.log(topKFrequentBucketSort([7, 7, 5, 7, 5, 6], 2))
