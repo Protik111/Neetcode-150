aca55fc31cc74ca115f4f26948a1ba2e10c9a213
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2026-05-02 12:59:18 +0600

    feat: product self optimized

--- 01-arrays-and-hashing/product-except-self/solution.ts
+++ 01-arrays-and-hashing/product-except-self/solution.ts
@@ -1,18 +1,38 @@
 // O(n^2) time complexity
-function productExceptSelf(nums: number[]): number[] {
-  const answers: number[] = [];
-  for (let i = 0; i < nums.length; i++) {
-    let product = 1;
-    for (let j = 0; j < nums.length; j++) {
-      if (i !== j) {
-        product *= nums[j];
-      }
-    }
+// function productExceptSelf(nums: number[]): number[] {
+//   const answers: number[] = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     let product = 1;
+//     for (let j = 0; j < nums.length; j++) {
+//       if (i !== j) {
+//         product *= nums[j];
+//       }
+//     }
 
-    answers[i] = product;
+//     answers[i] = product;
+//   }
+
+//   return answers;
+// }
+
+// O(n) time complexity
+function productExceptSelf(nums: number[]): number[] {
+  const n = nums.length;
+  let answer = new Array(n).fill(1);
+
+  //prefix
+  for (let i = 0; i < n; i++) {
+    answer[i] = answer[i - 1] * nums[i - 1] || 1;
   }
 
-  return answers;
+  //suffix
+  let right = 1;
+  for (let i = n - 1; i >= 0; i--) {
+    answer[i] *= right;
+    right *= nums[i];
+  }
+
+  return answer;
 }
 
 const nums = [1, 2, 3, 4];
